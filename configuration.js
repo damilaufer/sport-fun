e04b00238422e11e8e0ffd1bc1ebfcb9c39076fa
Damian Laufer <dalaufer@paypal.com>
2023-05-05 10:30:58 +0300

    Add group url

--- configuration.js
+++ configuration.js
@@ -2,10 +2,9 @@ function getPaymentUrl(form) {
   switch (form) {
     case 'manui':
       return 'https://private.invoice4u.co.il/newsite/he/clearing/public/i4u-clearing?ProductGuid=bce6561a-edc3-4f03-bc3b-a369a03b50ce'
-    // case 'group':
-    // return 'https://private.invoice4u.co.il/newsite/he/clearing/public/i4u-clearing?ProductGuid=eae795f9-4e8c-4878-b9d4-aec05177e28a'
+    case 'group':
+      return 'https://private.invoice4u.co.il/newsite/he/clearing/public/i4u-clearing?ProductGuid=442b03ec-e69e-4c90-8e89-237b77cc0bf0'
     default:
-      // Today we all have 5%
       return 'https://private.invoice4u.co.il/newsite/he/clearing/public/i4u-clearing?ProductGuid=eae795f9-4e8c-4878-b9d4-aec05177e28a'
   }
 }
