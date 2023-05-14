898a73fcc679268d4f4e3cca7da11b4553ced3e8
Damian Laufer <dalaufer@paypal.com>
2023-05-14 12:51:18 +0300

    and again

--- pages/api/register.js
+++ pages/api/register.js
@@ -87,8 +87,8 @@ export default async function handler(req, res) {
       res.status(500).json({ message })
     } else {
       const json = await response.json() // parses JSON response into native JavaScript objects
-      res.status(200).json(json)
       await sendRegistrationMail(kid.email, kid.firstName, kid.lastName)
+      res.status(200).json(json)
     }
   } catch (error) {
     console.log(error)
