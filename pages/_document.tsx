bd98d243249e62d099c1a2703f21a053739a20c5
Damian Laufer <dalaufer@paypal.com>
2025-04-19 22:33:22 +0300

    Bigger font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,11 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 import packageJson from '../package.json'
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html style={{ fontSize: '22px' }}>
         <Head />
         <body dir="rtl">
           <div style={{ display: 'none' }}>{packageJson.version}</div>
